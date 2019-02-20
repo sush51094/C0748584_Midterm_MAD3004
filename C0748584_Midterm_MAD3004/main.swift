4bf9eaadd4c3c0a909bc8e3b28b0bcb92e0c6ff9
MacStudent <macstudent@126-iMac12.local>
2019-02-20 18:27:57 -0500

    Initial Commit

---
+++ C0748584_Midterm_MAD3004/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  C0748584_Midterm_MAD3004
+//
+//  Created by MacStudent on 2019-02-20.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
